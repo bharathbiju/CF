acbb356f818e7f87899c62c9214d008e0b04d98d
Bharath Biju <bharathinfoclg@gmail.com>
2020-07-11 14:36:28 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,13 +1,6 @@
 // Initialize Firebase (ADD YOUR OWN DATA)
 var firebaseConfig = {
-  apiKey: "AIzaSyBnWGQfM677TswYMBUDMCVSC1CtuswL5Fo",
-  authDomain: "login-95de5.firebaseapp.com",
-  databaseURL: "https://login-95de5.firebaseio.com",
-  projectId: "login-95de5",
-  storageBucket: "login-95de5.appspot.com",
-  messagingSenderId: "277584537988",
-  appId: "1:277584537988:web:9446b819f007e75d4215d1",
-  measurementId: "G-S2DDKPEH1J"
+ //api
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
